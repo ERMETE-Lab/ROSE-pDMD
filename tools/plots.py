c912679f3f13ebcbc38c5cb2ac1675699edffe10
Steriva <stefano18.riva@mail.polimi.it>
2025-02-25 18:52:30 +0100

    Update plot to avoid squeezing for colorbars

--- tools/plots.py
+++ tools/plots.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import imageio.v2 as imageio
 import os
+import matplotlib.gridspec as gridspec
 
 from .backends import get_mag_fenicsx, get_mag
 
@@ -217,49 +218,100 @@ class PlotFlowCyl():
 
         return plot
 
-    def plotting_reconstruction(   self, params_to_plot, fom, Re_numbers, fom_times, tt,
-                                    recons: list, labels: list, cmap=cm.RdYlBu_r, 
-                                    length_plot = 6, levels=100, fontsize=20, shrink=0.9,
-                                    show_residuals = True
-                                ):
+    # def plotting_reconstruction(   self, params_to_plot, fom, Re_numbers, fom_times, tt,
+    #                                 recons: list, labels: list, cmap=cm.RdYlBu_r, 
+    #                                 length_plot = 6, levels=100, fontsize=20, shrink=0.9,
+    #                                 show_residuals = True
+    #                             ):
+
+    #     if show_residuals:
+    #         nrows = 1 + len(recons) * 2
+    #     else:
+    #         nrows = 1 + len(recons)
+    #     ncols = len(params_to_plot)
+
+    #     fig, axs = plt.subplots(nrows, ncols, figsize=(length_plot * ncols, nrows * (length_plot-0.5) * self.aspect))
+
+    #     for i, mu_i in enumerate(params_to_plot):
+
+    #         fom_cont = self.plot_contour(axs[0, i], fom[mu_i, tt], cmap=cmap, levels=levels)
+
+    #         for j, recon in enumerate(recons):
+    #             recon = recon[i]
+    #             self.plot_contour(axs[1 + j, i], recon[:, tt], cmap=cmap, levels=levels)
+
+    #             if show_residuals:
+    #                 if self.is_fenicsx:
+    #                     resid = np.abs(get_mag_fenicsx(fom[mu_i, tt]) - get_mag_fenicsx(recon[:, tt])) 
+    #                 else:
+    #                     resid = np.abs(get_mag(fom[mu_i, tt]) - get_mag(recon[:, tt]))
+
+    #                 self.plot_contour(axs[1 + len(recons) + j, i], resid, cmap=cmap, levels=levels)
+
+    #     fig.colorbar(fom_cont, ax=axs[:, -1], shrink=shrink).set_label('Velocity')
+
+    #     [axs[0, i].set_title('Re = {:.2f}'.format(Re_numbers[params_to_plot[i]]), fontsize=fontsize) for i, mu_i in enumerate(params_to_plot)]
+
+    #     axs[0,0].set_ylabel('FOM', fontsize=fontsize-5)
+    #     for j, recon in enumerate(recons):
+    #         axs[1 + j, 0].set_ylabel(labels[j], fontsize=fontsize-5)
+    #         if show_residuals:
+    #             axs[1 + len(recons) + j, 0].set_ylabel('Residual - ' + labels[j], fontsize=fontsize-5)
+
+    #     fig.suptitle('Time = {:.2f} s'.format(fom_times[tt]), y=.98, fontsize=fontsize+2)
+
+    #     return fig, axs
+
+    def plotting_reconstruction(self, params_to_plot, fom, Re_numbers, fom_times, tt,
+                                recons: list, labels: list, cmap=cm.RdYlBu_r, 
+                                length_plot=6, levels=100, fontsize=20, show_residuals=True):
 
         if show_residuals:
             nrows = 1 + len(recons) * 2
         else:
             nrows = 1 + len(recons)
-        ncols = len(params_to_plot)
+        ncols = len(params_to_plot) + 1  # Extra column for the colorbar
 
-        fig, axs = plt.subplots(nrows, ncols, figsize=(length_plot * ncols, nrows * (length_plot-0.5) * self.aspect))
+        fig = plt.figure(figsize=(length_plot * (ncols - 1), nrows * (length_plot - 0.5) * self.aspect))
+        gs = gridspec.GridSpec(nrows, ncols, width_ratios=[1] * (ncols - 1) + [0.05])  # Last column for colorbar
+
+        axs = np.empty((nrows, ncols - 1), dtype=object)
 
         for i, mu_i in enumerate(params_to_plot):
-
+            axs[0, i] = fig.add_subplot(gs[0, i])
             fom_cont = self.plot_contour(axs[0, i], fom[mu_i, tt], cmap=cmap, levels=levels)
 
             for j, recon in enumerate(recons):
-                recon = recon[i]
-                self.plot_contour(axs[1 + j, i], recon[:, tt], cmap=cmap, levels=levels)
+                axs[1 + j, i] = fig.add_subplot(gs[1 + j, i])
+                self.plot_contour(axs[1 + j, i], recon[i][:, tt], cmap=cmap, levels=levels)
 
                 if show_residuals:
+                    axs[1 + len(recons) + j, i] = fig.add_subplot(gs[1 + len(recons) + j, i])
                     if self.is_fenicsx:
-                        resid = np.abs(get_mag_fenicsx(fom[mu_i, tt]) - get_mag_fenicsx(recon[:, tt])) 
+                        resid = np.abs(get_mag_fenicsx(fom[mu_i, tt]) - get_mag_fenicsx(recon[i][:, tt])) 
                     else:
-                        resid = np.abs(get_mag(fom[mu_i, tt]) - get_mag(recon[:, tt]))
+                        resid = np.abs(get_mag(fom[mu_i, tt]) - get_mag(recon[i][:, tt]))
 
                     self.plot_contour(axs[1 + len(recons) + j, i], resid, cmap=cmap, levels=levels)
 
-        fig.colorbar(fom_cont, ax=axs[:, -1], shrink=shrink).set_label('Velocity')
-
-        [axs[0, i].set_title('Re = {:.2f}'.format(Re_numbers[params_to_plot[i]]), fontsize=fontsize) for i, mu_i in enumerate(params_to_plot)]
-
-        axs[0,0].set_ylabel('FOM', fontsize=fontsize-5)
+        # Create a dedicated colorbar axis
+        cax = fig.add_subplot(gs[:, -1])
+        cb = fig.colorbar(fom_cont, cax=cax)
+        cb.set_label('Velocity', fontsize=fontsize)
+        cb.ax.tick_params(labelsize=fontsize-4)
+        
+        # Titles and labels
+        [axs[0, i].set_title(f'Re = {Re_numbers[params_to_plot[i]]:.2f}', fontsize=fontsize) for i, mu_i in enumerate(params_to_plot)]
+        axs[0, 0].set_ylabel('FOM', fontsize=fontsize - 5)
         for j, recon in enumerate(recons):
-            axs[1 + j, 0].set_ylabel(labels[j], fontsize=fontsize-5)
+            axs[1 + j, 0].set_ylabel(labels[j], fontsize=fontsize - 5)
             if show_residuals:
-                axs[1 + len(recons) + j, 0].set_ylabel('Residual - ' + labels[j], fontsize=fontsize-5)
+                axs[1 + len(recons) + j, 0].set_ylabel(f'Residual - {labels[j]}', fontsize=fontsize - 5)
 
-        fig.suptitle('Time = {:.2f} s'.format(fom_times[tt]), y=.98, fontsize=fontsize+2)
+        fig.suptitle(f'Time = {fom_times[tt]:.2f} s', y=0.98, fontsize=fontsize + 2)
 
         return fig, axs
+
 
 
 class PlotTWIGL():
